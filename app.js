303560a2120923b2562b3cb5011470241458bdf1
Yusuf Kader <usertest@Yusufs-MacBook-Air.local>
2022-09-07 22:51:39 +0100

    NC-News-4 up for review

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const { sendAllTopics } = require('./controllers/topics.controllers');
+const { getArticlesById } = require('./controllers/articles.controllers')
+
+
+const app = express ();
+
+app.get("/api/topics", sendAllTopics);
+
+app.get("/api/articles/:article_id", getArticlesById);
+
+app.use((err, req, res, next) => {
+   if (err.status && err.message ) {
+    return res.status(err.status).send({ message: err.message});
+   }
+   else if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid data type" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Article not found" });
+  } else next(err);
+
+   return res.status(500).send({ message: "Internal Server Error"})
+})
+
+
+
+
+module.exports = app;
---
+++ controllers/articles.controllers.js
@@ -0,0 +1,24 @@
+
+const {selectArticlesById} = require("../models/articles.model")  
+
+exports.getArticlesById = (req, res, next) => {
+    const { article_id } = req.params;
+    selectArticlesById(article_id)
+    .then((article) => {
+    
+        
+        res.status(200).send({ article })
+    
+    })
+    .catch(next);
+};
+
+exports.getArticlesById = (req, res, next) => {
+    const {article_id} = req.params;
+    selectArticlesById (article_id)
+    .then((article) => {
+        res.status(200).send({article})
+    })
+    .catch(next);
+
+}
